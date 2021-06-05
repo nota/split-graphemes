c11487d72a0c79d77fc6608f96956894f19915de
Sho Hashimoto <hashimoto@shokai.org>
2021-06-05 14:47:45 +0900

    fix test

--- test/japanese-kana.js
+++ test/japanese-kana.js
@@ -4,11 +4,15 @@ import { japaneseKana } from '../lib/japanese-kana'
 import { testBreak } from './helper'
 
 describe('WordBreakJapaneseHiragana', function () {
-  it('break correctly', function () {
-    const regExp = new RegExp(japaneseKana, 'gu')
+  const regExp = new RegExp(japaneseKana, 'gu')
+
+  it('breaks plain kana', function () {
     testBreak(regExp, 'こんにちは', ['こ', 'ん', 'に', 'ち', 'は'])
-    testBreak(regExp, 'ごん゙に゙ぢば', ['ご', 'ん゙', 'に゙', 'ぢ', 'ば'])
     testBreak(regExp, 'ハヒフヘホ', ['ハ', 'ヒ', 'フ', 'ヘ', 'ホ'])
+  })
+
+  it('breaks kana with soundmark', function () {
+    testBreak(regExp, 'ごん゙に゙ぢば', ['ご', 'ん゙', 'に゙', 'ぢ', 'ば'])
     testBreak(regExp, 'パピプペポ', ['パ', 'ピ', 'プ', 'ペ', 'ポ'])
   })
 })
