a69f994a27cd7b38e1fdb834dc8b376bf6043314
Sho Hashimoto <hashimoto@shokai.org>
2021-06-05 14:04:50 +0900

    add japanese-kana pattern

---
+++ src/japanese-kana.js
@@ -0,0 +1,9 @@
+// spec
+// Hiragana https://www.unicode.org/charts/PDF/U3040.pdf
+// Katakana https://www.unicode.org/charts/PDF/U30A0.pdf
+
+const hiragana = '[\\u{3041}-\\u{3096}\\u{309D}-\\u{309F}]'
+const katakana = '[\\u{30A0}-\\u{30FF}]'
+const voiceSoundMark = '[\\u{3099}-\\u{309C}]'
+
+export const japaneseKana = `(${katakana}|${hiragana})${voiceSoundMark}?`
